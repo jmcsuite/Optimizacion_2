b235bf875e06d629b1e70d451f279c008d6b6a21
jmcsuite <jmcsuite@hotmail.com>
2022-10-10 10:13:22 -0500

    Added jacobian and progress eo ev programing

--- EvolutionaryProgramming/evolutionary_programming.py
+++ EvolutionaryProgramming/evolutionary_programming.py
@@ -11,6 +11,7 @@ class EvolutionaryProgramming:
         self.fitness = None
         self.rng = rng
         self.nfev = 0
+        self.progress = []
        
     def init_population(self):
         nvar = len(self.bounds)
@@ -53,25 +54,38 @@ class EvolutionaryProgramming:
         self.population = two_populations[order[:self.population.shape[0]]]
         self.fitness = two_fitness[order[:self.population.shape[0]]]
         
-    
-    def solve(self, max_it=1000):
+    def get_jacobian(self, solution):
+        epsilon = 0.001
+        fn = self.objective_func(solution, *self.args)
+        jac = np.zeros((self.bounds.shape[0]))
+        for i in range(len(jac)):
+            solution2 = solution.copy()
+            solution2[i] += epsilon
+            f2 = self.objective_func(solution2, *self.args)
+            jac[i] = (f2-fn)/epsilon
+        return jac
+
+    def solve(self, max_it=2000):
         self.init_population()
         elite_fx = self.fitness[0]
         elite_sol = self.population[0]
         nit = 0
+        self.progress.append(elite_fx)
         while nit < max_it and elite_fx > 0:
             new_population = self.mutation() 
             self.survivor_selection(new_population)
             elite_fx = self.fitness[0]
             elite_sol = self.population[0]     
             nit += 1
+            self.progress.append(elite_fx)
 
-        ans = {'x': elite_sol, 'nit': nit, 'fun': elite_fx, 'nfev': 0}                                
+        jac = self.get_jacobian(elite_sol)
+        ans = {'x': elite_sol, 'nit': nit, 'fun': elite_fx, 'nfev': self.nfev, 'jac': jac}                                
         # x: solution
         # nit: generation count
         # fun: fitness of the fittest
         # nfev: call count to objective function
-        return ans
+        return ans, self.progress
 
 def evolutionary_programming(func, bounds, args=(), popsize=30):
     print('Evolutionary Programming')
